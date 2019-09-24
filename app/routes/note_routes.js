29d48357da13f116ef1f3faabd380944b626e701
Andrei Shevchenko <andrei.zp.shevchenko@gmail.com>
2019-09-24 18:13:44 +0300

    init

---
+++ app/routes/note_routes.js
@@ -0,0 +1,55 @@
+var ObjectID = require('mongodb').ObjectID;
+module.exports = function(app, db) {
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                console.log(item);
+                res.send({'error': err});
+            } else {
+                console.log(item);
+                res.send(item);
+            }
+        });
+    });
+    app.post('/notes', (req, res) => {
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                console.log(result);
+                res.send({'error': err});
+            } else {
+                console.log(result);
+                res.send(result.ops[0]);
+            }
+        });
+    });
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('notes').remove(details, (err, item) => {
+            if (err) {
+                console.log(item);
+                res.send({'error': err});
+            } else {
+                console.log(item);
+                res.send('Note ' + id + ' deleted!');
+            }
+        });
+    });
+    app.put ('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').update(details, note, (err, result) => {
+            if (err) {
+                console.log(result);
+                res.send({'error': err});
+            } else {
+                console.log(result);
+                res.send(note);
+            }
+        });
+    });
+};
---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : 'mongodb+srv://shv:s2s2s2@oicluster-afs81.mongodb.net/test?retryWrites=true&w=majority\n'
+};
